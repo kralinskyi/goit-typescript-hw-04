2e34d4c11ae69855cc5703f54289bdb41249ad01
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-04-12 12:43:01 +0300

    new

---
+++ src/homework/1.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from "react";
+
+type Props = {
+  children: React.ReactNode;
+  onContentEndVisible: () => void;
+};
+
+export function Observer({ children, onContentEndVisible }: Props) {
+  const endContentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const options: IntersectionObserverInit = {
+      rootMargin: "0px",
+      threshold: 1.0,
+      root: null,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+          onContentEndVisible();
+          observer.disconnect();
+        }
+      });
+    }, options);
+
+    if (endContentRef.current) {
+      observer.observe(endContentRef.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [onContentEndVisible]);
+
+  return (
+    <div>
+      {children}
+      <div ref={endContentRef} />
+    </div>
+  );
+}
